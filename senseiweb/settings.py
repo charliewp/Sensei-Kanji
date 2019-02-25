c184486fcf3eb60d0c4e88de786c3f4da8bd5322
charliewp <cprice9739@carolina.rr.com>
2019-02-25 09:18:28 -0500

    import settings

--- senseiweb/settings.py
+++ senseiweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,7 +127,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'guestbook/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'kanji/static/media')
 
 LOGGING = {
     'version': 1,
