aa7758ac06f0efd56fe65f645818d7689b3f4c09
charliewp <cprice9739@carolina.rr.com>
2019-02-27 22:17:14 -0500

    update

--- kanji/views.py
+++ kanji/views.py
@@ -120,7 +120,7 @@ def node(request):
         
         location = "{0}  Node:{1}".format(node.location.description, node.name)
         
-        timediffstr = str(td.days) + "D " + str(td.seconds // 3600) + "H " + str(td.seconds // 60 % 60) + "M"
+        timediffstr = str(td.days) + "d " + str(td.seconds // 3600) + "h " + str(td.seconds // 60 % 60) + "m"
         
         return render(request, 'node.html',  {'location': location, 'timedelta': timediffstr, 'chartdefs': chartdefs, 'data': data })
           
