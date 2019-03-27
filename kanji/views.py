df487074688b06e9b0b7d5d898f7d606f417ed43
charliewp <cprice9739@carolina.rr.com>
2019-03-27 12:33:40 -0400

    update

--- kanji/views.py
+++ kanji/views.py
@@ -159,6 +159,7 @@ def slack(request):
    payload = json.loads(payload['payload'][0])
    
    action = payload["actions"][0]
+   print(action)
    print("Action={0} on device {1}".format(action["value"], action["action_id"]))
    #sc = SlackClient(_SLACK_TOKEN)
    #response = sc.api_call("chat.postMessage", channel=_CHANNEL_NAME, blocks=blockmessage)
