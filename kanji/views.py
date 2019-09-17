b1d8d2718d2b9d89d75774af3778433a0d1fdc28
charliewp <cprice9739@carolina.rr.com>
2019-09-17 10:34:07 -0400

    update

--- kanji/views.py
+++ kanji/views.py
@@ -300,7 +300,7 @@ def slack(request):
    payload = request.body.decode("utf-8")
    payload = urllib.parse.parse_qs(payload)
    payload = json.loads(payload['payload'][0])
-   log.error(payload["actions"])
+   log.error(payload)
    log.error("payload {0}".format(payload["actions"][0]))
    action = payload["actions"][0]
    log.error(action)
